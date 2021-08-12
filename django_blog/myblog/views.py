4e952d035c3e688d50966879021acadba8f7b41f
Daniil Moiseev <ksusonic@yandex.ru>
2021-08-12 23:49:28 +0300

    Pagination

--- django_blog/myblog/views.py
+++ django_blog/myblog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views import View
+from django.core.paginator import Paginator
 from .models import Post
 
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
         posts = Post.objects.all()
-        return render(request, 'myblog/home.html', context={
-            'posts': posts
+        paginator = Paginator(posts, 6)
+
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        return render(request, 'index.html', context={
+            'page_obj': page_obj
         })
